98d1c0dbab7115500d8124783efdbc15cb267a52
Orime112.github.io <1126439586@qq.com>
2020-06-23 14:38:29 +0800

    update from gridea: 2020-06-23 14:38:29

--- media/js/index.js
+++ media/js/index.js
@@ -1,13 +1,57 @@
-/**
- * Created by 95751 on 2018/8/3.
- */
-//获取高度赋值
-var height=document.documentElement.clientHeight;
-$('#centerbg').css('height',height+'px');
-
-//判断是否为ios系统
-var u = navigator.userAgent;
-var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
-if(isiOS==true){
-    $('#centerbg').css('background-attachment','scroll');
-}
+   function init() {
+        menu = $('.menu');
+        side = $('.side');
+        main = $('#main');
+        var bgchoice=$('#bgchoice').html();
+        var bg = $('#bg');
+        var bgurl = document.getElementById("bgurl").innerHTML;
+        if(bgchoice=='default')
+            for (var i = 0; i < 3; i++)
+                bgurl = bgurl.replace("\\", "/");
+        bg.css("background", "url('" + bgurl + "')");
+        postinfo = $('.postinfo');
+        postimage = $('.postimage');
+        tab1 = document.querySelector("#tab1");
+        ClicksideState = 0;
+        ClickbarState = 0;
+        maindom = document.querySelector('#main')
+        mainstyle = window.getComputedStyle(maindom, null)
+        mainleft = mainstyle.left;
+        posttitle = $('.posttitle');
+        posttitle2 = $('.posttitlewithoutimg');
+    }
+    init();
+    function menu_openclose1() {
+        if (ClicksideState == 0) {
+            side.animate({
+                'left': 0
+            });
+            main.css('width', '60%');
+            postimage.css('width', '100%');
+            postinfo.css('margin-left', '0%');
+            posttitle.css('margin-left', '0');
+            posttitle2.css('margin-left', '0');
+            main.animate({
+                'left': mainleft
+            })
+            ClicksideState = 1;
+        } else {
+            side.animate({
+                'left': -1200
+            });
+            main.css('width', '100%');
+            postimage.css('width', '60%');
+            postinfo.css('margin-left', '20%');
+            posttitle.css('margin-left', '20%');
+            posttitle2.css('margin-left', '20%');
+            main.animate({
+                'left': 0
+            })
+            ClicksideState = 0;
+        }
+        (tab1.className == 'tab') ?
+        tab1.className = 'tab active':
+            tab1.className = 'tab';
+    }
+    tab1.addEventListener('click', menu_openclose1);
+    menu_openclose1();
